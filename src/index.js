0440deff679d7ea1d46be65ae32b8ce5f3e629ca
cyberavian <cyberavian025@gmail.com>
2021-10-13 08:44:54 -0400

    added basic movement

--- src/index.js
+++ src/index.js
@@ -9,18 +9,36 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
+        this.canvas = this.sys.game.canvas;
+        this.viewHeight = window.innerHeight;
+        this.viewWidth = window.innerWidth;
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         this.load.image('background', 'src/assets/background.png')
         this.load.image('obstacle', 'src/assets/obstacle.png');
-        this.load.image('winny', 'src/assets/winny.png');
+        this.load.spritesheet('winny', 'src/assets/winny.png', { frameWidth: 16, frameheight: 16});
     }
 
     create ()
-
     {
+        let scale = (this.canvas.height / 2 - 64) / 16;
+        
         const background = this.add.image(180, 320, 'background');
-        const obstacle = this.add.image(300, 120, 'obstacle').setScale(2);
-        const winny = this.add.image(100, 120, 'winny').setScale(2);
 
+        this.obstacles = this.physics.add.group();
+        this.obstacles.create(this.canvas.width - 16, (scale * 16 / 2), 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width - 16, (this.canvas.height / 2 + 32) + (scale * 16 / 2), 'obstacle').setScale(4, scale);
+
+        const winny = this.physics.add.sprite(50, this.canvas.height / 2, 'winny').setScale(2);
+        winny.setCollideWorldBounds(true);
+        winny.setGravityY(1000);
+
+        this.cursors.space.on('down', () => {winny.setVelocityY(-300);});
+    }
+
+    update ()
+    {
+        this.obstacles.setVelocityX(-300);
     }
 }
 
@@ -28,6 +46,13 @@ const config = {
     type: Phaser.AUTO,
     width: 360,
     height: 640,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // gravity: {y: 1000},
+            fps: 60
+        }
+    },
     scene: MyGame
 };
 
