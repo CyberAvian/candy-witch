f42a2c5a1c82f66a3cea64c007386e036a797c61
cyberavian <cyberavian025@gmail.com>
2021-10-14 16:32:11 -0400

    added infinite obstacles and scrolling

--- src/index.js
+++ src/index.js
@@ -21,24 +21,46 @@ class MyGame extends Phaser.Scene
 
     create ()
     {
-        let scale = (this.canvas.height / 2 - 64) / 16;
+        let scale = (this.canvas.height / 2 - 32) / 16;
         
         const background = this.add.image(180, 320, 'background');
 
         this.obstacles = this.physics.add.group();
-        this.obstacles.create(this.canvas.width - 16, (scale * 16 / 2), 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width - 16, (this.canvas.height / 2 + 32) + (scale * 16 / 2), 'obstacle').setScale(4, scale);
 
-        const winny = this.physics.add.sprite(50, this.canvas.height / 2, 'winny').setScale(2);
-        winny.setCollideWorldBounds(true);
-        winny.setGravityY(1000);
+        let obstacleTopY = (scale * 16 / 2);
+        let obstacleBottomY = (scale * 16 / 2) + (this.canvas.height / 2 + 64)
+        this.obstacles.create(this.canvas.width + 256, obstacleTopY, 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width + 256, obstacleBottomY, 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width + 512, obstacleTopY, 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width + 512, obstacleBottomY, 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width + 768, obstacleTopY, 'obstacle').setScale(4, scale);
+        this.obstacles.create(this.canvas.width + 768, obstacleBottomY, 'obstacle').setScale(4, scale);
 
-        this.cursors.space.on('down', () => {winny.setVelocityY(-300);});
+        this.winny = this.physics.add.sprite(50, this.canvas.height / 2, 'winny').setScale(2);
+        this.winny.setCollideWorldBounds(true);
+        this.winny.setGravityY(1000);
+
+        this.cursors.space.on('down', () => {this.winny.setVelocityY(-300);});
     }
 
     update ()
     {
-        this.obstacles.setVelocityX(-300);
+        for (let index = 0, length = this.obstacles.getLength(); index < length; index++) {
+            let obstacle = this.obstacles.getChildren()[index];
+            obstacle.setVelocityX(-150);
+        }
+
+        for (let index = 0, length = this.obstacles.getLength(); index < length; index++) {
+            let obstacle = this.obstacles.getChildren()[index];
+            let x = obstacle.body.x;
+            if (x <= -64) {
+                let leadingPosition = (index + 2) % 6;
+                console.log(`Index ${index} Leading Position ${leadingPosition}`);
+                let leadingObstacle = this.obstacles.getChildren()[leadingPosition];
+                let leadingObstacleX = leadingObstacle.body.x;
+                obstacle.body.x = leadingObstacleX + 256;
+            }
+        }
     }
 }
 
