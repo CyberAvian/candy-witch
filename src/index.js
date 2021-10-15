67261a1638aa42fc1f6829b2bf15ac6ce3c4170d
cyberavian <cyberavian025@gmail.com>
2021-10-15 15:29:59 -0400

    created varying heights

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 class MyGame extends Phaser.Scene
 {
+    PAIRS = 3;
+
     constructor ()
     {
         super();
@@ -21,20 +23,16 @@ class MyGame extends Phaser.Scene
 
     create ()
     {
-        let scale = (this.canvas.height / 2 - 32) / 16;
-        
         const background = this.add.image(180, 320, 'background');
 
         this.obstacles = this.physics.add.group();
 
-        let obstacleTopY = (scale * 16 / 2);
-        let obstacleBottomY = (scale * 16 / 2) + (this.canvas.height / 2 + 56)
-        this.obstacles.create(this.canvas.width + 576, obstacleTopY, 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width + 576, obstacleBottomY, 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width + 800, obstacleTopY, 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width + 800, obstacleBottomY, 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width + 1024, obstacleTopY, 'obstacle').setScale(4, scale);
-        this.obstacles.create(this.canvas.width + 1024, obstacleBottomY, 'obstacle').setScale(4, scale);
+        for (let index = 0; index < this.PAIRS; index++) {
+            let offset = 256 * (index + 1);
+            this.createObstacles(offset);
+        }
+
+        this.obstacleChildren = this.obstacles.getChildren();
 
         this.winny = this.physics.add.sprite(50, this.canvas.height / 2, 'winny').setScale(2);
         this.winny.setCollideWorldBounds(true);
@@ -42,33 +40,76 @@ class MyGame extends Phaser.Scene
 
         this.cursors.space.on('down', () => {this.winny.setVelocityY(-300);});
 
-        for (let index = 0, length = this.obstacles.getLength(); index < length; index++) {
-            let obstacle = this.obstacles.getChildren()[index];
+        for (let index = 0, length = this.obstacleChildren.length; index < length; index++) {
+            let obstacle = this.obstacleChildren[index];
             obstacle.setVelocityX(-150);
         }
-
     }
 
     update ()
     {   
+        this.listenLeavePlayArea();
+    }
+
+    createObstacles(offset) {
+        let widthScale = 64 / 16;
+        let heights = this.getScaledObstacleHeights();
+        let obstacleX = this.canvas.width + offset;
+        let obstacleTopY = heights[0] * 16 / 2;
+        let obstacleBottomY = this.canvas.height - (heights[1] * 16 / 2);
+
+        this.obstacles.create(obstacleX, obstacleTopY, 'obstacle').setScale(widthScale, heightScaleTop);
+        this.obstacles.create(obstacleX, obstacleBottomY, 'obstacle').setScale(widthScale, heightScaleBottom);
+    }
+
+    getScaledObstacleHeights() {
+        let spaceBetween = 56;
+        // The height of the top obstacle should be between 1/4 of the canvas height and 3/4 of the canvas height.
+        // However, we are accounting for the space between the top and bottom obstacles in this height.
+        // Therefore, the height really is between 1/4 the canvas and 3/4 the canvas minus the space between height. 
+        let maxHeight = (this.canvas.height * 3 / 4) - spaceBetween
+        let topHeight = Math.floor(Math.random() * maxHeight) + (this.canvas.height / 4);
+        if (topHeight < (this.canvas.height / 4)) topHeight = this.canvas.height / 4;
+        let bottomHeight = this.canvas.height - topHeight - spaceBetween;
+
+        return [topHeight, bottomHeight];
+    }
+
+    listenLeavePlayArea() {
         for (let index = 0, length = this.obstacles.getLength(); index < length; index++) {
-            let obstacle = this.obstacles.getChildren()[index];
-            let x = obstacle.body.x;
-            if (x <= -64) {
-                let leadingPosition = (index + 2) % 6;
-                console.log(`Index ${index} Leading Position ${leadingPosition}`);
-                let leadingObstacle = this.obstacles.getChildren()[leadingPosition];
-                let leadingObstacleX = leadingObstacle.body.x;
-                obstacle.body.x = leadingObstacleX + 224;
+            if (this.checkOutsidePlayArea(index)) {
+                let obstacle = this.obstacleChildren[index];
+                this.resetObstacle(obstacle, index);
             }
         }
     }
 
-    createObstacle() {
-        let width = 64;
-        let spaceBetween = 56;
-        let topHeight = Math.floor( Math.random() * ( this.canvas.height * 3 / 4 ) );
-        let bottomHeight = this.canvas.height - ( topHeight + spaceBetween );
+    checkOutsidePlayArea(index) {
+        let obstacle = this.obstacles.getChildren()[index];
+        let x = obstacle.body.x;
+        if (x <= -64) {
+            return true;
+        }
+        return false;
+    }
+
+    resetObstacles(index) {
+        let topObstacle = this.obstacleChildren[index];
+        let bottomObstacle = this.obstacleChildren[index + 1];
+        let heights = this.getScaledObstacleHeights();
+        let obstacleTopHeight = heights[0] * 16 / 2;
+        let obstacleBottomHeight = this.canvas.height - (heights[1] * 16 / 2);
+
+        this.setObstaclePosition(index, topObstacle, heights[0]);
+        this.setObstaclePosition(index + 1, bottomObstacle, heights[1]);
+    }
+
+    setObstaclePosition(index, obstacle, height) {
+        let leadObstacleIndex = (index + this.PAIRS + 1) % (this.PAIRS * 2);
+        let leadObstacle = this.obstacleChildren[leadObstacleIndex];
+        
+        obstacle.body.x = leadObstacle.body.x + 224;
+        ;
     }
 }
 
